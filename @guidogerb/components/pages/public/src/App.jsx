42f5bcec5a61a739aa4f66ce390bd67e3af1f707
Gary Gerber <guidogerb1966@outlook.com>
2025-09-01 18:26:52 -0600

    Refactor import paths in App and main components for consistency

--- @guidogerb/components/pages/public/src/App.jsx
+++ @guidogerb/components/pages/public/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from '../../../src/assets/react.svg'
-import viteLogo from '/vite.svg'
-import '../../../src/App.css'
+import reactLogo from './assets/react.svg'
+import viteLogo from '../public/vite.svg'
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
