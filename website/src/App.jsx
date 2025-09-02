68964e3d1ab3f3bb8c7993371d5c92efb8759834
Gary Gerber <guidogerb1966@outlook.com>
2025-09-02 17:14:15 -0600

    Refactor App component to simplify layout and remove unused elements

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '../public/vite.svg'
 import './App.css'
 
 import { Auth, useAuth } from '@guidogerb/components-auth'
@@ -16,27 +13,13 @@ function ProtectedContent() {
 }
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-
       {/* Public section (always visible) */}
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          GuidoGerbPublishing Store!
         </p>
       </div>
 
@@ -48,9 +31,6 @@ function App() {
         </Auth>
       </div>
 
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
