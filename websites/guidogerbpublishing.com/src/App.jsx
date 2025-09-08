b769725e1fffb2ba38e6689120e0d620b6e8e64d
Gary Gerber <guidogerb1966@outlook.com>
2025-09-08 13:56:56 -0600

    Add initial project setup with React, Vite, and ESLint configuration

---
+++ websites/guidogerbpublishing.com/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="card">
+          <H3>Hello This Is My Story World</H3>
+      </div>
+    </>
+  )
+}
+
+export default App;
---
+++ websites/guidogerbpublishing.com/src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { AuthProvider } from '@guidogerb/components-auth'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+      <AuthProvider
+          authority={import.meta.env.VITE_COGNITO_AUTHORITY}
+          metadataUrl={import.meta.env.VITE_COGNITO_METADATA_URL}
+          client_id={import.meta.env.VITE_COGNITO_CLIENT_ID}
+          redirect_uri={import.meta.env.VITE_REDIRECT_URI}
+          response_type={import.meta.env.VITE_RESPONSE_TYPE}
+          scope={import.meta.env.VITE_COGNITO_SCOPE}
+          post_logout_redirect_uri={import.meta.env.VITE_COGNITO_POST_LOGOUT_REDIRECT_URI}
+          loginCallbackPath={import.meta.env.VITE_LOGIN_CALLBACK_PATH || '/auth/loginCallback'}
+      >
+          <App />
+      </AuthProvider>
+  </StrictMode>,
+)
