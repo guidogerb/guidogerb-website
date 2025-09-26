de8be74e7502e7b374cd336f9d3fb8293d373b3e
Gary Gerber <guidogerb1966@outlook.com>
2025-09-26 07:31:13 -0600

    feat(app): add automation scaffolding helpers

--- @guidogerb/components/app/index.js
+++ @guidogerb/components/app/index.js
@@ -1,3 +1,7 @@
 export { App, AppBasic, useAppApiClient, useAppBasicContext } from './src/App.jsx'
 export { App as default } from './src/App.jsx'
 export { APP_VARIANT_SPECS, getAppVariantSpec, listAppVariantSpecs } from './src/variantSpecs.js'
+export {
+  APP_BASIC_AUTOMATION_DEFAULTS,
+  createAppBasicAutomationScaffold,
+} from './src/automation.js'
